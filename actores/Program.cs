37afeb5b8dab44d0045f58ef5831ba561bea889c
KennyKZH <60447970+KennyKZH@users.noreply.github.com>
2020-03-10 23:19:39 -0700

    Add files via upload

---
+++ actores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace actores
+{
+    class actores
+    {
+        private string nom;
+        public actores(string n)
+        {
+            nom = n;
+        }
+        public void imprime()
+        {
+            Console.WriteLine(nom);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<actores> Actor = new List<actores>();
+            Actor.Add(new actores("Osamu Mukai"));
+            Actor.Add(new actores("Shota Matsuda"));
+            Actor.Add(new actores("Toma Ikuta"));
+            Actor.Add(new actores("Haruma Miura"));
+ 
+            foreach(actores a in Actor)
+            {
+                a.imprime();
+            }
+            
+        }
+    }
+}
